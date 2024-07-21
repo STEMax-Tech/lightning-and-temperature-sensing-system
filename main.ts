9234b32b8034e27211a263a648a87b24dd1875f6
NamVo <51428674+namvotech@users.noreply.github.com>
2024-07-21 13:09:44 +0700

    Edit code for microbit v1

--- main.ts
+++ main.ts
@@ -1,14 +1,11 @@
-let analogValue = 0
-let airHumi = 0
 let senLight = 0
+let airHumi = 0
 let airTemp = 0
+pins.digitalWritePin(DigitalPin.P14, 0)
+pins.digitalWritePin(DigitalPin.P15, 0)
 let setLight = EEPROM.readw(100)
 I2C_LCD1602.LcdInit(39)
-let strip = neopixel.create(DigitalPin.P0, 8, NeoPixelMode.RGB)
 basic.forever(function () {
-    serial.writeLine("Air Temp: " + airTemp + " C")
-    serial.writeLine("Air Humi: " + airTemp + " %")
-    serial.writeLine("Light: " + senLight + " %")
     I2C_LCD1602.ShowString("Te:" + Math.round(airTemp) + "C  ", 0, 0)
     I2C_LCD1602.ShowString("Hu:" + airHumi + "%  ", 9, 0)
     I2C_LCD1602.ShowString("Li:" + senLight + "%  ", 0, 1)
@@ -16,36 +13,21 @@ basic.forever(function () {
     basic.pause(100)
 })
 basic.forever(function () {
-    if (senLight < setLight) {
+    if (senLight <= setLight) {
         pins.digitalWritePin(DigitalPin.P14, 1)
-        strip.showColor(neopixel.colors(NeoPixelColors.Violet))
+        pins.digitalWritePin(DigitalPin.P15, 1)
         basic.showIcon(IconNames.Asleep)
     } else {
         pins.digitalWritePin(DigitalPin.P14, 0)
-        strip.clear()
+        pins.digitalWritePin(DigitalPin.P15, 0)
         basic.showIcon(IconNames.Happy)
     }
 })
 basic.forever(function () {
     airTemp = Environment.dht11value(Environment.DHT11Type.DHT11_temperature_C, DigitalPin.P2)
-    basic.pause(100)
+    basic.pause(200)
     airHumi = Environment.dht11value(Environment.DHT11Type.DHT11_humidity, DigitalPin.P2)
-    basic.pause(100)
-})
-basic.forever(function () {
-    analogValue = 0
-    for (let index = 0; index <= 9; index++) {
-        index += pins.analogReadPin(AnalogPin.P1)
-    }
-    analogValue = analogValue / 10
-    senLight = Math.round(pins.map(
-    analogValue,
-    0,
-    1023,
-    100,
-    0
-    ))
-    basic.pause(100)
+    basic.pause(200)
 })
 // button state
 basic.forever(function () {
@@ -54,10 +36,9 @@ basic.forever(function () {
             basic.showIcon(IconNames.Yes)
         }
         EEPROM.writew(100, setLight)
-    }
-    if (input.buttonIsPressed(Button.A)) {
+    } else if (input.buttonIsPressed(Button.A)) {
         setLight += -1
-        if (senLight < 0) {
+        if (setLight < 0) {
             setLight = 100
         }
         basic.showLeds(`
@@ -69,7 +50,7 @@ basic.forever(function () {
             `)
     } else if (input.buttonIsPressed(Button.B)) {
         setLight += 1
-        if (senLight > 100) {
+        if (setLight > 100) {
             setLight = 0
         }
         basic.showLeds(`
@@ -80,4 +61,14 @@ basic.forever(function () {
             . . . . .
             `)
     }
+})
+basic.forever(function () {
+    senLight = Math.round(pins.map(
+    pins.analogReadPin(AnalogPin.P1),
+    0,
+    1023,
+    100,
+    0
+    ))
+    basic.pause(200)
 })
